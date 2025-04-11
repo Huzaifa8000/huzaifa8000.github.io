695994647f21a68de5d957872a00913e9b57c341
Huzaifa8000 <hsiddiqui3@my.waketech.edu>
2025-04-11 11:25:00 -0400

    Created gradebook html file and javascript file

---
+++ gradebook.js
@@ -0,0 +1,13 @@
+function fetchGradeData() {
+
+    console.log("Fetching grade data...");
+}
+
+function populateGradebook(data) {
+
+    console.log("Populating gradebook with data:", data);
+
+}
+    
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
